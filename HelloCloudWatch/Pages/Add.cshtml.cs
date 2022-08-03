4493ebe46d352616755d3654f72d3a52e1169968
Burak Gokoglu <burak.gokoglu@intouchsol.com>
2022-08-03 12:18:01 -0500

    HelloCloudWatch

---
+++ HelloCloudWatch/Pages/Add.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloCloudWatch.Pages;
+
+public class AddModel : PageModel
+{
+    private readonly ILogger<AddModel> _logger;
+    public int N1 { get; set; }
+    public int N2 { get; set; }
+    public int Sum { get; set; }
+
+    public AddModel(ILogger<AddModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet(int n1, int n2)
+    {
+        _logger.LogInformation("Add n1={N1}, n2={N2}", n1, n2);
+        N1 = n1;
+        N2 = n2;
+        Sum = n1 + n2;
+        _logger.LogInformation("Sum: {Sum}", Sum);
+    }
+}
